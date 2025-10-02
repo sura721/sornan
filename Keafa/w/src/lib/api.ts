92023b2503a580dd97964abaa59c7da1973a49db
sura721 <surafeladmas721@gmail.com>
2025-10-02 18:41:41 -0300

    auth

--- Keafa/w/src/lib/api.ts
+++ Keafa/w/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
  const backendUrl = import.meta.env.VITE_API_BASE_URL;
 
- const API_URL = backendUrl || 'http://localhost:5000/api';
+ const API_URL =  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 console.log(`API is configured to use base URL: ${API_URL}`);
 
